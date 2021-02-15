ed2183e218d1d132d2bbc68cd8569ac29483fc88
lockerboxer9089 <79079765+lockerboxer9089@users.noreply.github.com>
2021-02-15 20:20:39 +0530

    Update Australia_Bowling_Simulator.py

--- Australia_Bowling_Simulator.py
+++ Australia_Bowling_Simulator.py
@@ -1,14 +1,26 @@
 import random
-class Bowler:
-	def __init__(self, name, speed, length):
-		self.name = name
-		self.speed = speed
-		self.length = length
-	def mainfunc(self):
-		print("Bowler is :", self.name, "speed = ", self.speed, "length is :", self.length)
-for i in range(0,6):
-	a = random.randrange(145, 150)
-	x = random.randrange(0,100)
-	if x in range(0,30):
-		P1 = Bowler("Cummmins", a, "good")
-		P1.mainfunc()
+class bowler:
+    def __init__(self, name, ballspeed, balllength):
+        self.name = name
+        self.ballspeed = ballspeed
+        self.balllength = balllength
+    def mainfunc(self):
+        print("The bowler is :", self.name, "The ball speed is :", self.ballspeed, "The length of the ball is :", self.balllength)
+
+def cumminsover():
+    for i in range(0,8):
+        x = random.randrange(0,100)
+        y = random.randrange(138,147)
+        if x in range(0,30):
+            PatCummins = bowler("Pat Cummins", y, "Short ball")
+            PatCummins.mainfunc
+        elif x in range(30,80):
+            PatCummins = bowler("Pat Cummins", y, "Good length ball")
+            PatCummins.mainfunc()
+        elif x in range(80,100):
+            PatCummins = bowler("Pat Cummins", y, "overpitched ball")
+            PatCummins.mainfunc()
+        else:
+            print("ERROR")  
+
+cumminsover()
