9a5ca2eea6272497cc4dfdd33736c95f4e67a6dd
lockerboxer9089 <79079765+lockerboxer9089@users.noreply.github.com>
2021-02-17 09:02:13 +0530

    Update Australia_Bowling_Simulator.py

--- Australia_Bowling_Simulator.py
+++ Australia_Bowling_Simulator.py
@@ -2,26 +2,32 @@ from getpass import getpass
 import random
 
 class bowler:
-    def __init__(self, bowlername, bowlerspeed, bowlerball):
-        self.bowlername = bowlername
-        self.bowlerspeed = bowlerspeed
-        self.bowlerball = bowlerball
+    def __init__(bowlerobject, name, ballspeed, balllength):
+        bowlerobject.name = name
+        bowlerobject.ballspeed = ballspeed
+        bowlerobject.balllength = balllength
 
-    def bowlaball(self):
-        print("Bowler = ", self.bowlername, "Speed = ", self.bowlerspeed, "Ball length = ", self.bowlerball)
+    def bowlaball(abc):
+        print("Bowler = ", abc.name, ":ball speed = ", abc.ballspeed, ":Length of the ball = ", abc.balllength)
 
-def mainfunc():
-    x = random.randrange  
-    if x in range(0,30):
-        PatCummins = bowler("Pat Cummins", y, "Short ball")
-        PatCummins.bowlaball
-    elif x in range(30,80):
-        PatCummins = bowler("Pat Cummins", y, "Good length ball")
-        PatCummins.bowlaball()
-    elif x in range(80,100):
-        PatCummins = bowler("Pat Cummins", y, "Overpitched ball")
-        PatCummins.bowlaball()
-user_input = getpass("Enter bowler name: ").lower()
-if user_input == "cummins":
-    mainfunc()
+def bowlanovercummins():
+    for i in range(0,6):
+        x = random.randrange(0,100)
+        y = random.randrange(133,147)
+        if x in range(0,30):
+            PatCummins = bowler("Pat Cummins", y, "Short ball")
+            PatCummins.bowlaball()
+        elif x in range(30,80):
+            PatCummins = bowler("Pat Cummins", y, "Good length ball")
+            PatCummins.bowlaball()
+        elif x in range(80,100):
+            PatCummins = bowler("Pat Cummins", y, "overpitched ball")
+            PatCummins.bowlaball()
 
+def bowlanoverhazlewood():
+    for i in range(0,6):
+        x = random.randrange(0,100)
+        y = random.randrange(131,143)
+        if x in range(0,40):
+            JoshHazlewood = bowler("Josh Hazlewood", y, "Short ball")
+            JoshHazlewood.bowlaball()
