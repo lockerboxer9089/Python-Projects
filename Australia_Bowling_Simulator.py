9abf1f16bef24d4a17d4d076e5325010241cee4c
lockerboxer9089 <79079765+lockerboxer9089@users.noreply.github.com>
2021-02-17 10:30:53 +0530

    Update Australia_Bowling_Simulator.py

--- Australia_Bowling_Simulator.py
+++ Australia_Bowling_Simulator.py
@@ -31,3 +31,46 @@ def bowlanoverhazlewood():
         if x in range(0,40):
             JoshHazlewood = bowler("Josh Hazlewood", y, "Short ball")
             JoshHazlewood.bowlaball()
+        elif x in range(40,90):
+            JoshHazlewood = bowler("Josh Hazlewood", y, "Good length ball")
+            JoshHazlewood.bowlaball()
+        elif x in range(90,100):
+            JoshHazlewood = bowler("Josh Hazlewood", y, "Overpitched")
+            JoshHazlewood.bowlaball()
+
+def bowlanoverstarc():
+    for i in range(0,6):
+        x = random.randrange(0,100)
+        y = random.randrange(141,150)
+        if x in range(0,30):
+            MitchellStarc = bowler("Mitchell Starc", y, "Short ball:")
+            MitchellStarc.bowlaball()
+        elif x in range(30,70):
+            MitchellStarc = bowler("Mitchell Starc", y, "Good length ball")
+            MitchellStarc.bowlaball()
+        elif x in range(70,100):
+            MitchellStarc = bowler("Mitchell Starc", y, "Overpitched ball")
+            MitchellStarc.bowlaball()
+    
+print("""
+INFORMATION:
+This program will simulate 1 over from any bowler of your choice. 
+It will tell the bowler's name, speed of the ball bowled and the langth bowled.
+You can choose any of the names that the program gives as options.
+""")
+while True:
+    user_input = getpass("Enter bowler name: Options = Cummins, Hazlewood and Starc : Enter 'done' to stop the simulator: ").lower()
+    if user_input == "cummins":
+        bowlanovercummins()
+    elif user_input == "hazlewood":
+        bowlanoverhazlewood()
+    elif user_input == "starc":
+        bowlanoverstarc()
+    elif user_input == "done":
+        break
+    else:
+        print("""
+ERROR:
+Wrong name entered.
+Pls enter one of the names that are in the options given by the program!
+""")
