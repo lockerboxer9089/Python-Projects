e9c3fb99913bed32a5eb1cb5600a5b9151fa5488
lockerboxer9089 <79079765+lockerboxer9089@users.noreply.github.com>
2021-02-25 21:50:08 +0530

    Update Australia_Bowling_Simulator.py

--- Australia_Bowling_Simulator.py
+++ Australia_Bowling_Simulator.py
@@ -1,77 +1,55 @@
-from getpass import getpass
 import random
-# Importing required libraries
-class bowler: # Creating a bowler class
-    def __init__(bowlerobject, name, ballspeed, balllength):
-        bowlerobject.name = name
-        bowlerobject.ballspeed = ballspeed
-        bowlerobject.balllength = balllength
 
-    def bowlaball(abc):
-        print("Bowler = ", abc.name, ":ball speed = ", abc.ballspeed, ":Length of the ball = ", abc.balllength)
+class Bowler:
+	def __init__(self, name, ProbShort, ProbGood, ProbOverP, ProbWicket):
+		self.name = name
+		self.ProbShort = ProbShort
+		self.ProbGood = ProbGood
+		self.ProbOverP = ProbOverP
+		self.ProbWicket = ProbWicket
+	
+	def BowlABall(self):
+		CumulShort = self.ProbShort
+		CumulGood = CumulShort + self.ProbGood
+		CumulOverP = CumulGood + self.ProbOverP
 
-def bowlanovercummins(): #Creating a function to bowl 1 over from Cummins
-    for i in range(0,6):
-        x = random.randrange(0,100)
-        y = random.randrange(133,147)
-        if x in range(0,30):
-            PatCummins = bowler("Pat Cummins", y, "Short ball")
-            PatCummins.bowlaball()
-        elif x in range(30,80):
-            PatCummins = bowler("Pat Cummins", y, "Good length ball")
-            PatCummins.bowlaball()
-        elif x in range(80,100):
-            PatCummins = bowler("Pat Cummins", y, "overpitched ball")
-            PatCummins.bowlaball()
+		length = random.uniform(0,1)
+		if length < CumulShort:
+			print("Ball bowled is a short ball")
+		elif length < CumulGood:
+			print("Ball bowled is a good length ball")
+		elif length < CumulOverP:
+			print("Ball bowled is an overpitched ball")
+		else:
+			print("Error!")
 
-def bowlanoverhazlewood(): #Creating a function to bowl 1 over from hazlewood
-    for i in range(0,6):
-        x = random.randrange(0,100)
-        y = random.randrange(131,143)
-        if x in range(0,40):
-            JoshHazlewood = bowler("Josh Hazlewood", y, "Short ball")
-            JoshHazlewood.bowlaball()
-        elif x in range(40,90):
-            JoshHazlewood = bowler("Josh Hazlewood", y, "Good length ball")
-            JoshHazlewood.bowlaball()
-        elif x in range(90,100):
-            JoshHazlewood = bowler("Josh Hazlewood", y, "Overpitched")
-            JoshHazlewood.bowlaball()
+		wicket = random.uniform(0,1)
+		BALLS = 0
+		if wicket > self.ProbWicket:
+			print("No wicket this ball")
+			BALLS = BALLS + 1
+		elif wicket <= self.ProbWicket:
+			print("Wicket!, Number of balls before previous wicket = ", BALLS)
+			BALLS = 0
 
-def bowlanoverstarc(): #Creating a function to bowl 1 over from starc
-    for i in range(0,6):
-        x = random.randrange(0,100)
-        y = random.randrange(141,150)
-        if x in range(0,30):
-            MitchellStarc = bowler("Mitchell Starc", y, "Short ball:")
-            MitchellStarc.bowlaball()
-        elif x in range(30,70):
-            MitchellStarc = bowler("Mitchell Starc", y, "Good length ball")
-            MitchellStarc.bowlaball()
-        elif x in range(70,100):
-            MitchellStarc = bowler("Mitchell Starc", y, "Overpitched ball")
-            MitchellStarc.bowlaball()
-    
-print(""" 
-INFORMATION:
-This program will simulate 1 over from any bowler of your choice. 
-It will tell the bowler's name, speed of the ball bowled and the langth bowled.
-You can choose any of the names that the program gives as options.
-""") #Giving some information about the program
-while True: #Main part of the program
-    user_input = getpass("Enter bowler name: Options = Cummins, Hazlewood and Starc : Enter 'done' to stop the simulator: ").lower()
-    if user_input == "cummins":
-        bowlanovercummins()
-    elif user_input == "hazlewood":
-        bowlanoverhazlewood()
-    elif user_input == "starc":
-        bowlanoverstarc()
-    elif user_input == "done":
-        break
-    else:
-        print("""
-ERROR:
-Wrong name entered.
-Pls enter one of the names that are in the options given by the program!
-""")
-#End of program
+
+PatCummins = Bowler("Pat Cummins", 0.3, 0.5, 0.2, 0.05)
+JoshHazlewood = Bowler("Josh Hazlewood", 0.4, 0.5, 0.1, 0.03)
+MitchellStarc = Bowler("Mitchell Starc", 0.25, 0.4, 0.35, 0.04)
+NathanLyon = Bowler("Nathan Lyon", 0.2, 0.6, 0.2, 0.06)
+while True:
+	user_input = str(input("Enter bowler name, Enter 'done' to end the simulator, Option = Cummins, Hazlewood, Starc and Lyon -")).lower()
+	if user_input == 'cummins':
+		for i in range(0,6):
+			PatCummins.BowlABall()
+	elif user_input == 'hazlewood':
+		for i in range(0,6):
+			JoshHazlewood.BowlABall()
+	elif user_input == 'starc':
+		for i in range(0,6):
+			MitchellStarc.BowlABall()
+	elif user_input == 'lyon':
+		for i in range(0,6):
+			NathanLyon.BowlABall()
+	elif user_input == 'done':
+		break
